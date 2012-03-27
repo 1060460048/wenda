3d7a319104b92a878edd1462ed5cbea481cc9d84
Ricky Feng <ricky.feng@163.com>
2012-03-27 18:04:40 +0800

    ricky.feng@163.com

--- library/RFLib/View/Helper/Substr.php
+++ library/RFLib/View/Helper/Substr.php
@@ -1,22 +1,17 @@
 <?php
 class RFLib_View_Helper_Substr extends Zend_View_Helper_Abstract
 {
-    public function substr($string, $limit = 100, $after = '...') 
+    public function substr($string, $limit = 100, $charset='UTF-8', $after = '...') 
     {
     	$string = trim($string);
     	
         if (null == $string) {
             return '';
         }
-        if (strlen($string) <= $limit) {
+        if (mb_strlen($string) <= $limit) {
             return $string;
         }
-
-        if (function_exists('mb_substr')) {
-            return mb_substr($string, 0, $limit, 'UTF-8') . $after;
-        } else {
-            return substr($string, 0, $limit) . $after;
-        }
+        return mb_substr($string, 0, $limit, $charset) . $after;
     }
 
 }
