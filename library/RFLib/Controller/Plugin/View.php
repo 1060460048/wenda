4711296903b92edb54fd83c6302b3778a7f34070
Ricky Feng <ricky.feng@163.com>
2012-04-25 11:07:48 +0800

    add notice of license

--- library/RFLib/Controller/Plugin/View.php
+++ library/RFLib/Controller/Plugin/View.php
@@ -1,10 +1,36 @@
 <?php
 /**
+ * RFLib
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to ricky.feng@163.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Wenda to newer
+ * versions in the future. If you wish to customize Wenda for your
+ * needs please refer to https://www.github.com/rickyfeng/wenda.git for more information.
+ *
+ * @category    RFLib
+ * @package     RFLib_Controller
+ * @copyright   Copyright (c) 2010 Ricky Feng (ricky.feng@163.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
  * View plugin class
  * 
- * @copyright  Copyright (c) 2011 Ricky Feng (http://code.google.com/p/rphp4zf)
- * @license    New BSD License
- */
+ * @author      Ricky Feng <ricky.feng@163.com>
+ * @version     $Id: View.php 2011-06-12 19:08:44Z Ricky Feng $ * 
+ */ 
+ 
 class RFLib_Controller_Plugin_View extends Zend_Controller_Plugin_Abstract
 {
     public function routeShutdown(Zend_Controller_Request_Abstract $request)
--- library/RFLib/Core.php
+++ library/RFLib/Core.php
@@ -1,23 +1,30 @@
 <?php
-/**
+ /**
  * RFLib
  *
- * LICENSE
+ * NOTICE OF LICENSE
  *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://www.gnu.org/licenses/gpl-2.0.txt
+ * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to ricky.feng@163.com so we can send you a copy immediately.
  *
- * @category	RFLib
- * @package		RFLib_Core
- * @copyright	Copyright (c) 2009-2012 Ricky Feng (ricky.feng@163.com)
- * @license		http://www.gnu.org/licenses/gpl-2.0.txt GNU GENERAL PUBLIC LICENSE Version 2
- * @version		$Id: Core.php 2010-05-14 19:08:44Z Ricky Feng $
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Wenda to newer
+ * versions in the future. If you wish to customize Wenda for your
+ * needs please refer to https://www.github.com/rickyfeng/wenda.git for more information.
+ *
+ * @category    RFLib
+ * @package     RFLib_Core
+ * @copyright   Copyright (c) 2010 Ricky Feng (ricky.feng@163.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version     $Id: Core.php 2010-05-14 19:08:44Z Ricky Feng $ 
  */
+ 
 final class RFLib_Core
 {
     /**
